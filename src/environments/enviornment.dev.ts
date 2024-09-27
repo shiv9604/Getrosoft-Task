aef159170974c13fe131a464e67ae392d2673f63
Shivprasad Kounsalye <shivkounsalye@gmail.com>
2024-09-27 14:56:33 +0530

    Implement register user feature

---
+++ src/environments/enviornment.dev.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    baseUrl: 'assets/data', // Assets Dir URL
+    // Other variables...
+};
---
+++ src/environments/enviornment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    baseUrl: 'https://mp3c73caecbe80b61f49.free.beeceptor.com/api', // Hosted API URL
+    // Other variables...
+};
